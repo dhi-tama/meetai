3087087a37cb89e0a83209c542b78ec486a7e266
dhi-tama <andika.ap757@gmail.com>
2025-08-14 18:10:53 +0900

    12: Agent Data Table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,14 +5,25 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { columns } from "../components/colomns";
+import { DataTable } from "../components/data-table";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  //
-
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState
+          title="Your Personal Mentor Awaits"
+          description="Build an AI tutor who teaches at your pace, explains clearly, and never runs out of patience."
+        />
+      )}
+    </div>
+  );
 };
 
 export const AgentsViewLoading = () => {
