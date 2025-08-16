02b2ebc0c93f3d69df89cb083cace73d89e717a0
dhi-tama <andika.ap757@gmail.com>
2025-08-16 09:05:02 +0900

    16: Meetings Setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const MeetingsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Deploying Your Agent"
+      description="Collecting intelligence from the field."
+    />
+  );
+};
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Signal Lost"
+      description="Something went off-script. Adjusting the plan."
+    />
+  );
+};
